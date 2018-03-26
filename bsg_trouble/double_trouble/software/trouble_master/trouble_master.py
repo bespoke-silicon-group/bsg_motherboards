9e34c75e16f8fad5a2b35aba7d3ed846d58fc750
Paul <zig053@eng.ucsd.edu>
2018-03-25 23:55:03 -0700

    docs for double trouble added

---
+++ bsg_trouble/double_trouble/software/trouble_master/trouble_master.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2014-2015 UCSD Bespoken Systems Group
+# All rights reserved
+# Created by Shengye Wang <shengye@ucsd.edu>
+
+from console_util import *
+from console_board import console_board
+from console_dig_pot import console_voltage_power
+from console_cur_mon import console_report_current
+
+prompt_main = '''
+-------------------- Trouble Master --------------------
+1. Board Version
+2. Set Voltage and Power
+3. Measure Current
+4. Reset Communication
+5. Miscellaneous
+6. ASIC Emulator Master OFF
+7. Program Digital Potential Meter EEPROM
+8. Program Digital Potential Meter EEPROM With Current Voltage
+0. Exit
+-------------------- Trouble Master --------------------
+'''
+
+misc_info = '''
+-------------------- Bespoken Silicon Group --------------------
+--------------------  Trouble Master V1.00  --------------------
+'''
+
+while (True):
+    print prompt_main
+    try:
+        cmd = raw_input('Please make a selection: ')
+        try:
+            cmd = int(cmd)
+        except:
+            print "Unknown command."
+            continue
+        if (cmd == 1):
+            console_board()
+        elif (cmd == 2):
+            console_voltage_power()
+        elif (cmd == 3):
+            console_report_current()
+        elif (cmd == 4):
+            ser.read(256)
+            ser.write([cmd_map['CMD_NOP']])
+            check_cmd_return()
+        elif (cmd == 5):
+            print misc_info
+        elif (cmd == 6):
+            ser.write([cmd_map['CMD_POWER_MASTER_OFF']])
+            check_cmd_return()
+        elif (cmd == 7):
+            ser.write([cmd_map['CMD_PROG_DIG_POT']])
+            check_cmd_return()
+        elif (cmd == 8):
+            ser.write([cmd_map['CMD_PROG_DIG_POT_VOLTAGE_NOW']])
+            check_cmd_return()
+        elif (cmd == 0):
+            exit(0)
+    except:
+        raise
+
