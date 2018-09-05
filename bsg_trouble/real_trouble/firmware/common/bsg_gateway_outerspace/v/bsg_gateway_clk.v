b9334ddc2b5422f2e9bb63dbf96643dacd9a1d65
Paul Gao <gaozihou@uw.edu>
2018-09-05 00:49:37 -0700

    safe constraints

--- bsg_trouble/real_trouble/firmware/common/bsg_gateway_outerspace/v/bsg_gateway_clk.v
+++ bsg_trouble/real_trouble/firmware/common/bsg_gateway_outerspace/v/bsg_gateway_clk.v
@@ -55,7 +55,7 @@ module bsg_gateway_clk
   // 150MHz * (dcm_mult_lp/dcm_io_master_clk_divide_lp)
   // if dcm_mult_lp=7 and dcm_io_master_clk_divide_lp=9,
   // then (int/ext) io_master_clk_o are 66.67MHz
-  localparam pll_core_clk_divide_lp = 5;
+  localparam pll_core_clk_divide_lp = 7;
   localparam pll_fast_core_clk_divide_lp = 5;
   localparam pll_io_master_clk_1x_divide_lp = 10;
   localparam pll_io_master_clk_2x_divide_lp = 5;
