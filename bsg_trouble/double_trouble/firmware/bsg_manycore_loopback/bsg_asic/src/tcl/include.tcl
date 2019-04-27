d83de83c05ad93d1367799378f54d6d652a82f68
Paul Gao <gaozihou@uw.edu>
2019-04-26 18:05:28 -0700

    manycore loopback test moved to github

--- bsg_trouble/double_trouble/firmware/bsg_manycore_loopback/bsg_asic/src/tcl/include.tcl
+++ bsg_trouble/double_trouble/firmware/bsg_manycore_loopback/bsg_asic/src/tcl/include.tcl
@@ -1,9 +1,9 @@
 set bsg_asic_dir $::env(BSG_ASIC_DIR)
-set bsg_ip_cores_dir $bsg_asic_dir/out/bsg_ip_cores
+set basejump_stl_dir $bsg_asic_dir/out/basejump_stl
 set bsg_manycore_dir $bsg_asic_dir/out/bsg_manycore
 
 set AC_RTL_INCLUDE [join "
-  $bsg_ip_cores_dir/bsg_misc
-  $bsg_ip_cores_dir/bsg_noc
+  $basejump_stl_dir/bsg_misc
+  $basejump_stl_dir/bsg_noc
   $bsg_manycore_dir/v
 "]
--- bsg_trouble/double_trouble/firmware/common/bsg_gateway_manycore_loopback/tcl/filelist.tcl
+++ bsg_trouble/double_trouble/firmware/common/bsg_gateway_manycore_loopback/tcl/filelist.tcl
@@ -1,54 +1,54 @@
 set bsg_fpga_ip_dir $::env(BSG_FPGA_IP_DIR)
 set bsg_fpga_firmware_dir $::env(BSG_FPGA_FIRMWARE_DIR)
 set bsg_gateway_dir $::env(BSG_GATEWAY_DIR)
-set bsg_ip_cores_dir $bsg_gateway_dir/out/bsg_ip_cores
+set basejump_stl_dir $bsg_gateway_dir/out/basejump_stl
 set bsg_designs_dir $bsg_gateway_dir/out/bsg_designs
 set bsg_gateway_target $::env(BSG_GATEWAY_TARGET)
 
 set GW_RTL_FILES [join "
-  $bsg_ip_cores_dir/bsg_misc/bsg_defines.v
-  $bsg_ip_cores_dir/bsg_async/bsg_async_fifo.v
-  $bsg_ip_cores_dir/bsg_async/bsg_async_ptr_gray.v
-  $bsg_ip_cores_dir/bsg_async/bsg_sync_sync.v
-  $bsg_ip_cores_dir/bsg_async/bsg_launch_sync_sync.v
-  $bsg_ip_cores_dir/bsg_async/bsg_async_credit_counter.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_scan.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_mux.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_circular_ptr.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_gray_to_binary.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_binary_plus_one_to_gray.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_wait_cycles.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_thermometer_count.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_counter_up_down_variable.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_counter_clear_up.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_decode_with_v.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_decode.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_round_robin_arb.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_crossbar_o_by_i.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_mux_one_hot.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_clkgate_optional.v
-  $bsg_ip_cores_dir/bsg_misc/bsg_dlatch.v
-  $bsg_ip_cores_dir/bsg_mem/bsg_mem_1r1w_synth.v
-  $bsg_ip_cores_dir/bsg_mem/bsg_mem_1r1w.v
-  $bsg_ip_cores_dir/bsg_mem/bsg_mem_1rw_sync.v
-  $bsg_ip_cores_dir/bsg_mem/bsg_mem_1rw_sync_synth.v
-  $bsg_ip_cores_dir/bsg_dataflow/bsg_fifo_tracker.v
-  $bsg_ip_cores_dir/bsg_dataflow/bsg_fifo_1r1w_small.v
-  $bsg_ip_cores_dir/bsg_dataflow/bsg_two_fifo.v
-  $bsg_ip_cores_dir/bsg_dataflow/bsg_fifo_1r1w_large.v
-  $bsg_ip_cores_dir/bsg_dataflow/bsg_fifo_1rw_large.v
-  $bsg_ip_cores_dir/bsg_dataflow/bsg_channel_tunnel.v
-  $bsg_ip_cores_dir/bsg_dataflow/bsg_channel_tunnel_in.v
-  $bsg_ip_cores_dir/bsg_dataflow/bsg_channel_tunnel_out.v
-  $bsg_ip_cores_dir/bsg_dataflow/bsg_1_to_n_tagged_fifo.v
-  $bsg_ip_cores_dir/bsg_dataflow/bsg_1_to_n_tagged.v
-  $bsg_ip_cores_dir/bsg_dataflow/bsg_round_robin_n_to_1.v
-  $bsg_ip_cores_dir/bsg_dataflow/bsg_round_robin_2_to_2.v
-  $bsg_ip_cores_dir/bsg_dataflow/bsg_serial_in_parallel_out.v
-  $bsg_ip_cores_dir/bsg_dataflow/bsg_fifo_1r1w_pseudo_large.v
-  $bsg_ip_cores_dir/bsg_test/test_bsg_data_gen.v
-  $bsg_ip_cores_dir/bsg_noc/bsg_noc_pkg.v
-  $bsg_ip_cores_dir/bsg_noc/bsg_wormhole_router.v
+  $basejump_stl_dir/bsg_misc/bsg_defines.v
+  $basejump_stl_dir/bsg_async/bsg_async_fifo.v
+  $basejump_stl_dir/bsg_async/bsg_async_ptr_gray.v
+  $basejump_stl_dir/bsg_async/bsg_sync_sync.v
+  $basejump_stl_dir/bsg_async/bsg_launch_sync_sync.v
+  $basejump_stl_dir/bsg_async/bsg_async_credit_counter.v
+  $basejump_stl_dir/bsg_misc/bsg_scan.v
+  $basejump_stl_dir/bsg_misc/bsg_mux.v
+  $basejump_stl_dir/bsg_misc/bsg_circular_ptr.v
+  $basejump_stl_dir/bsg_misc/bsg_gray_to_binary.v
+  $basejump_stl_dir/bsg_misc/bsg_binary_plus_one_to_gray.v
+  $basejump_stl_dir/bsg_misc/bsg_wait_cycles.v
+  $basejump_stl_dir/bsg_misc/bsg_thermometer_count.v
+  $basejump_stl_dir/bsg_misc/bsg_counter_up_down_variable.v
+  $basejump_stl_dir/bsg_misc/bsg_counter_clear_up.v
+  $basejump_stl_dir/bsg_misc/bsg_decode_with_v.v
+  $basejump_stl_dir/bsg_misc/bsg_decode.v
+  $basejump_stl_dir/bsg_misc/bsg_round_robin_arb.v
+  $basejump_stl_dir/bsg_misc/bsg_crossbar_o_by_i.v
+  $basejump_stl_dir/bsg_misc/bsg_mux_one_hot.v
+  $basejump_stl_dir/bsg_misc/bsg_clkgate_optional.v
+  $basejump_stl_dir/bsg_misc/bsg_dlatch.v
+  $basejump_stl_dir/bsg_mem/bsg_mem_1r1w_synth.v
+  $basejump_stl_dir/bsg_mem/bsg_mem_1r1w.v
+  $basejump_stl_dir/bsg_mem/bsg_mem_1rw_sync.v
+  $basejump_stl_dir/bsg_mem/bsg_mem_1rw_sync_synth.v
+  $basejump_stl_dir/bsg_dataflow/bsg_fifo_tracker.v
+  $basejump_stl_dir/bsg_dataflow/bsg_fifo_1r1w_small.v
+  $basejump_stl_dir/bsg_dataflow/bsg_two_fifo.v
+  $basejump_stl_dir/bsg_dataflow/bsg_fifo_1r1w_large.v
+  $basejump_stl_dir/bsg_dataflow/bsg_fifo_1rw_large.v
+  $basejump_stl_dir/bsg_dataflow/bsg_channel_tunnel.v
+  $basejump_stl_dir/bsg_dataflow/bsg_channel_tunnel_in.v
+  $basejump_stl_dir/bsg_dataflow/bsg_channel_tunnel_out.v
+  $basejump_stl_dir/bsg_dataflow/bsg_1_to_n_tagged_fifo.v
+  $basejump_stl_dir/bsg_dataflow/bsg_1_to_n_tagged.v
+  $basejump_stl_dir/bsg_dataflow/bsg_round_robin_n_to_1.v
+  $basejump_stl_dir/bsg_dataflow/bsg_round_robin_2_to_2.v
+  $basejump_stl_dir/bsg_dataflow/bsg_serial_in_parallel_out.v
+  $basejump_stl_dir/bsg_dataflow/bsg_fifo_1r1w_pseudo_large.v
+  $basejump_stl_dir/bsg_test/test_bsg_data_gen.v
+  $basejump_stl_dir/bsg_noc/bsg_noc_pkg.v
+  $basejump_stl_dir/bsg_noc/bsg_wormhole_router.v
   
   $bsg_fpga_firmware_dir/common/$bsg_gateway_target/v/bsg_manycore_async_link_to_wormhole.v
   $bsg_fpga_firmware_dir/common/$bsg_gateway_target/v/bsg_manycore_loopback_test_node.v
